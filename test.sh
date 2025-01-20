f8b0acbc64de036786adb9262927135f78db6d59
niyadev <jmanz5@hotmail.com>
2025-01-20 18:26:43 -0500

    Started working on Yaml

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 clear
 
-c3c compile-test bulk.c3l/*.c3 bulk.c3l/tests/*.c3 keybind.c3l/*.c3 keybind.c3l/tests/*.c3 raylib.c3l/*.c3 raylib.c3l/tests/*.c3 -l raylib
+c3c compile-test bulk.c3l/*.c3 bulk.c3l/tests/*.c3 keybind.c3l/*.c3 keybind.c3l/tests/*.c3 raylib.c3l/*.c3 raylib.c3l/tests/*.c3 yaml.c3l/*.c3 yaml.c3l/tests/*.c3 -l raylib
