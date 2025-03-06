6b20033bacb8701625c5a9c48c43ff539e54de56
niyadev <jmanz5@hotmail.com>
2025-03-06 16:08:37 -0500

    Work started on settings

--- test.sh
+++ test.sh
@@ -2,4 +2,10 @@
 
 clear
 
-c3c compile-test bulk.c3l/*.c3 bulk.c3l/tests/*.c3 keybind.c3l/*.c3 keybind.c3l/tests/*.c3 raylib.c3l/*.c3 raylib.c3l/tests/*.c3 yaml.c3l/*.c3 yaml.c3l/tests/*.c3 localization.c3l/*.c3 localization.c3l/tests/*.c3 -l raylib
+c3c compile-test bulk.c3l/*.c3 bulk.c3l/tests/*.c3 \
+  keybind.c3l/*.c3 keybind.c3l/tests/*.c3 \
+  raylib.c3l/*.c3 raylib.c3l/tests/*.c3 \
+  yaml.c3l/*.c3 yaml.c3l/tests/*.c3 \
+  localization.c3l/*.c3 localization.c3l/tests/*.c3 \
+  settings.c3l/*.c3 settings.c3l/tests/*c3 \
+  -l raylib
