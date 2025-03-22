1d5aae27208a6a57b00064afcedb5f3bba309b71
niyadev <jmanz5@hotmail.com>
2025-03-22 17:03:49 -0400

    Changed to compiling into dynamic library and trying to bug fix

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ clear
 
 c3c build -l raylib -q
 
-cp build/libpleroma.a pleroma.c3l/linux-x64/libpleroma.a
+#cp build/libpleroma.a pleroma.c3l/linux-x64/libpleroma.a
+cp build/libpleroma.so pleroma.c3l/linux-x64/libpleroma.so
 cp src/raylib/raylib.c3 pleroma.c3l/raylib_raw.c3
 
 
